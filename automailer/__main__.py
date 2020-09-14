d5ee6246d94ba52a4852a79b241fdb565e2f8489
Jacob Clarke <jacobclarke718@gmail.com>
2020-09-13 21:07:04 -0500

    updated to use click

--- automailer/__main__.py
+++ automailer/__main__.py
@@ -1,17 +1,4 @@
-import yagmail
-import csv
-from pathlib import Path
-from markdown2 import Markdown
-import frontmatter
+from automailer.client import mail
 
-with ((data_dir:= Path('data')) / 'email_list.csv').open(mode='r') as csv_file:
-    for row in csv.DictReader(csv_file):
-        message = frontmatter.load(data_dir / 'message.md')
-        yagmail.SMTP(
-                message['sender'],
-                password = message['password']
-            ).send(
-                to= row['email'],
-                subject=message['subject'],
-                contents=Markdown().convert(message.content.format(**row)), 
-        )
+if __name__ == "__main__":
+    mail()
